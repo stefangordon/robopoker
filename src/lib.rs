4ef0a1daec895f0e986e931de181198e3311b612
Stefan Gordon <stefan@stefangordon.com>
2025-05-24 13:58:18 -0500

    Update NLHE batch size and refactor best response logic in exploitability module
    - Increased `CFR_BATCH_SIZE_NLHE` from 128 to 512 for improved performance.
    - Refactored best response and strategy evaluation functions to utilize a recursive approach with node indexing.
    - Enhanced tree traversal logic by integrating node information during child node creation, optimizing the decision-making process.
    - Removed the `create_node` function to streamline code and improve clarity in node handling.

--- src/lib.rs
+++ src/lib.rs
@@ -60,7 +60,7 @@ const CFR_TREE_COUNT_RPS: usize = 8192;
 pub const CHECKPOINT_HOURS: u64 = 6;
 
 // nlhe mccfr parameters
-const CFR_BATCH_SIZE_NLHE: usize = 128;
+const CFR_BATCH_SIZE_NLHE: usize = 512;
 const CFR_TREE_COUNT_NLHE: usize = 0x1000000;
 
 /// profile average sampling parameters
